7fe44d8d88853e3e7cad7a5dd98f9f8e09fda14c
adamaripici <55006272+adamaripici@users.noreply.github.com>
2022-07-18 09:30:06 -0500

    added basic inputs to login page

--- capstone-ui/src/components/Login/Login.jsx
+++ capstone-ui/src/components/Login/Login.jsx
@@ -1,10 +1,71 @@
 import * as React from "react"
+import { useState } from "react"
+import { Link } from "react-router-dom"
 import "./Login.css"
 
 export default function Login() {
+    const [errors, setErrors] = useState({})
+    const [form, setForm] = useState({
+        email: "",
+        password: ""
+    })
+    const handleOnInputChange = (event) => {
+        if (event.target.name === "password") {
+          if (form.passwordConfirm && form.passwordConfirm !== event.target.value) {
+            setErrors((e) => ({ ...e, passwordConfirm: "Password's do not match" }))
+          } else {
+            setErrors((e) => ({ ...e, passwordConfirm: null }))
+          }
+        }
+        if (event.target.name === "passwordConfirm") {
+          if (form.password && form.password !== event.target.value) {
+            setErrors((e) => ({ ...e, passwordConfirm: "Password's do not match" }))
+          } else {
+            setErrors((e) => ({ ...e, passwordConfirm: null }))
+          }
+        }
+        if (event.target.name === "email") {
+          if (event.target.value.indexOf("@") === -1) {
+            setErrors((e) => ({ ...e, email: "Please enter a valid email." }))
+          } else {
+            setErrors((e) => ({ ...e, email: null }))
+          }
+        }
+    
+        setForm((f) => ({ ...f, [event.target.name]: event.target.value }))
+      }
     return (
         <div className="login">
             <h1>Login</h1>
+           <div className="input-field">
+                <label htmlFor="email">Email</label>
+                <input
+                    className="form-input"
+                    type="email"
+                    name="email"
+                    placeholder="user@gmail.com"
+                    value={form.email}
+                    onChange={handleOnInputChange}
+                />
+                {errors.email && <span className="error">{errors.email}</span>}
+           </div>
+           <div className="input-field">
+                <label htmlFor="password">Password</label>
+                <input
+                    className="form-input"
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    value={form.password}
+                    onChange={handleOnInputChange}
+                />
+                {errors.password && <span className="error">{errors.password}</span>}
+           </div>
+           <div className="footer">
+            <p>
+              Don't have an account? Sign up <Link to="/register">here</Link>
+            </p>
+          </div>
         </div>
     )
 }
