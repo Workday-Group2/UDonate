15a3a4c3ad727b93c71834e9f04c66e73720e254
adamaripici <55006272+adamaripici@users.noreply.github.com>
2022-07-20 13:04:22 -0500

    donation fixes

--- capstone-ui/src/components/DonationDetailPage/DonationDetailPage.jsx
+++ capstone-ui/src/components/DonationDetailPage/DonationDetailPage.jsx
@@ -1,17 +1,18 @@
 import * as React from "react"
 import "./DonationDetailPage.css"
 
-export default function DonationDetailPage(props) {
-    console.log("imahe",props)
+export default function DonationDetailPage({quantity,name,imageUrl,category,
+    id,avgRating,totalRatings,donation_desc,location,createdAt}) {
+    console.log("image",imageUrl)
     return (
         <div className="details">
-            <h1>Donation Detail</h1>
-            <img className="donation-image" src={props.imageUrl} alt="" />
+            {/* <h1>Donation Detail</h1> */}
+            <img className="donation-image" src={imageUrl} alt="" />
              <div className="nutrition-text">
-            <p className="nutrition-name">Title: {props.name}</p>
-            <p className="nutrition-category not-name">Category: {props.category}</p>
-            <p className="nutrition-quantity not-name">Quantity: {props.quantity}</p>
-            
+            <p className="nutrition-name">Title: {name}</p>
+            <p className="nutrition-category not-name">Category: {category}</p>
+            <p className="nutrition-quantity not-name">Quantity: {quantity}</p>
+             
             </div> 
         </div>
     )
