96903c86a2b924d90906b77b8b3f60c80e5ad72b
nkewden <nkewden@gmail.com>
2022-07-19 22:03:22 -0500

    added about page

--- capstone-ui/src/components/Home/Home.jsx
+++ capstone-ui/src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import * as React from "react"
 import "./Home.css"
 import Footer from "../Footer/Footer"
+import Contact from "../Contact/Contact"
 
 export default function Home() {
     return (
@@ -9,6 +10,7 @@ export default function Home() {
             <div className="image">
                 <img className="hero-img" src="https://img.freepik.com/free-vector/tiny-people-standing-near-box-donation-food-delivery-volunteers-giving-healthy-grocery-goods-charity-flat-vector-illustration-social-support-humanitarian-help-community-sharing-concept_74855-21023.jpg?w=2000"/>
             </div>
+            <Contact />
             <Footer />
         </div>
     )
