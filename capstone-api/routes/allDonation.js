20c2ba41cbe94e9eb8efdb931203f84269113afc
nkewden <nkewden@gmail.com>
2022-07-21 13:18:03 -0500

    completed booking in the backend and displaying all donation in the fronend

---
+++ capstone-api/routes/allDonation.js
@@ -0,0 +1,17 @@
+
+const express = require("express");
+const router = express.Router();
+const Donation = require("../models/donation")
+
+
+router.get("/", async (req, res, next) => {
+    try {
+        const {user} = res.locals;
+        const donations = await Donation.listDonationForAll({user})
+        return res.status(200).json({donations})
+    } catch(err) {
+        next(err)
+    }
+})
+
+module.exports = router
--- capstone-api/routes/donation.js
+++ capstone-api/routes/donation.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Donation = require("../models/donation")
 const security = require("../middleware/security")
 const Rating = require("../models/rating")
+const Booking = require("../models/booking")
 
 //router to create a donation
 router.post("/", security.requireAuthenticatedUser, async (req, res, next) => {
@@ -38,12 +39,24 @@ router.get("/", security.requireAuthenticatedUser, async (req, res, next) => {
     }
 })
 
+
 router.post("/:donationId/rating", security.requireAuthenticatedUser, async (req, res, next) => {
     try {
         const {donationId} = req.params
         const {user} = res.locals
         const rating = await Rating.createRatingForDonation({ rating: req.body.rating, user, donationId })
         return res.status(201).json({ rating })
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.post("/:donationId/newBooking", security.requireAuthenticatedUser, async (req, res, next) => {
+    try {
+        const {donationId} = req.params
+        const {user} = res.locals
+        const newBooking = await Booking.createBooking({ newBooking: req.body.newBooking, user, donationId })
+        return res.status(201).json({ newBooking })
     } catch(err) {
         next(err)
     }
