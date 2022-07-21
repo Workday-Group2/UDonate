93f2b6a56ecabc5a6bffdc86ce476a1cce9d70d6
nkewden <nkewden@gmail.com>
2022-07-21 11:24:53 -0500

    added booking for models and routes

--- capstone-api/app.js
+++ capstone-api/app.js
@@ -5,6 +5,7 @@ const { NotFoundError, BadRequestError} = require("./utils/errors")
 const authRoutes = require("./routes/auth")
 const security = require("./middleware/security")
 const donationRoutes = require("./routes/donation")
+const bookingRoutes = require("./routes/booking")
 
 const app = express()
 
@@ -18,6 +19,7 @@ app.use(security.extractUserFromJwt)
 
 app.use("/auth", authRoutes)
 app.use("/donation", donationRoutes)
+app.use("/booking", bookingRoutes)
 
 // app.get("/", async(req, res, next) => {
 //     res.status(200).json({ ping: "pong"})
--- capstone-api/models/booking.js
+++ capstone-api/models/booking.js
@@ -3,7 +3,54 @@ const { BadRequestError, NotFoundError } = require("../utils/errors")
 
 class Booking {
     static async createBooking ({newBooking, donation, user}) {
-        const requiredFields = []
+        const requiredFields = ["startDate"]
+        requiredFields.forEach((field) => {
+            if (!newBooking?.hasOwnProperty(field)) {
+                throw new BadRequestError(`Missing required field - ${field} - in request body.`)
+      }
+    })
+
+    const results = await db.query(
+        `
+          INSERT INTO booking (
+            start_date, 
+            donation_id, 
+            user_id
+          )
+          VALUES (
+            ($1)::date, 
+            $2,  
+            (SELECT id FROM users WHERE username = $7)
+          )
+          RETURNING id,
+                    start_date AS "startDate",
+                    donation_id AS "donationId",
+                    user_id AS "userId",
+                    (
+                      SELECT username 
+                      FROM users
+                      WHERE id = user_id
+                    ) AS "username",                  
+                    (
+                      SELECT users.username
+                      FROM users
+                      WHERE users.id = (
+                        SELECT donation.user_id
+                        FROM donation
+                        WHERE donation.id = donation_id
+                      )
+                    ) AS "donatorUsername",                  
+                    created_at AS "createdAt";
+        `,
+        [
+          newBooking.startDate,
+          donation.id,
+          user.username,
+        ]
+      )
+  
+      return results.rows[0]
+
     }
 }
 
