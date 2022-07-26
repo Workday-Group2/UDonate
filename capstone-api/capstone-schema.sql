c6342ec051a1c07e8296406e7426e655158240cd
nkewden <nkewden@gmail.com>
2022-07-26 14:35:31 -0500

    added booking review table in the db

--- capstone-api/capstone-schema.sql
+++ capstone-api/capstone-schema.sql
@@ -46,4 +46,11 @@ CREATE TABLE booking (
   
 );
 
+CREATE TABLE review (
+ id             SERIAL PRIMARY KEY,
+ user_id        INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+ comment       VARCHAR(1000) NOT NULL,
+ created_at     TIMESTAMP NOT NULL DEFAULT NOW(),
+ booking_comment_id    INTEGER NOT NULL REFERENCES booking(id) ON DELETE CASCADE
+);
 
